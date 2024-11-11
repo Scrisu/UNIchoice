ba2c51f452c90bffe2f4806172689c0d241931a4
Dani <scrisudaniel@gmail.com>
2024-11-12 00:46:09 +0200

    Update cu render

--- app.js
+++ app.js
@@ -1,41 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const sequelize = require('./config/db'); // Conexiunea la baza de date
 const userRoutes = require('./routes/userRoutes'); // Rutele pentru utilizatori
 
-// Middleware pentru parsing-ul JSON
-app.use(express.json());
-
-app.use(express.static('public'));
-
-const cors = require('cors');
-app.use(cors());
 
 
-// Conectare la baza de date
-sequelize.authenticate()
-  .then(() => {
-    console.log('Conexiunea la baza de date a fost stabilită cu succes.');
-  })
-  .catch(err => {
-    console.error('Eroare la conectarea la baza de date:', err);
-  });
 
-// Sincronizează baza de date
-sequelize.sync()
-  .then(() => {
-    console.log('Baza de date este sincronizată');
-  })
-  .catch(err => {
-    console.error('Eroare la sincronizarea bazei de date:', err);
-  });
 
 // Folosește rutele definite
 app.use('/api/users', userRoutes);
 
 // Pornire server
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Serverul rulează la http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
-const { Sequelize } = require('sequelize');
+const Sequelize = require('sequelize');
 
-// Inițializează conexiunea la baza de date folosind variabilele de mediu
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT, // Acesta va fi "postgres" sau "mysql"
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',  // Specify your database dialect here, e.g., 'postgres', 'mysql'
+    protocol: 'postgres', // Required if using a connection string
+    logging: false,       // Optional: Disable logging if you prefer
 });
 
 module.exports = sequelize;
