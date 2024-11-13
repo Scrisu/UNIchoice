2abafa96508876b885fab79769a12f85fee6d7a6
Dani <scrisudaniel@gmail.com>
2024-11-13 22:19:18 +0200

    dubla 3

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
-const { isAuthenticated } = require('../middleware/authMiddleware');
+const { isAuthenticated } = require('../middleware/authmiddleware');
 
 
 // Login Route
